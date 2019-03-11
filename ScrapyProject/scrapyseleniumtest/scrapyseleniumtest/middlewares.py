cb417aab46cc667a1aa28b359e04ab72cc02ae3b
Jamie33 <452070961@qq.com>
2019-03-11 10:07:55 +0800

    SPider

--- ScrapyProject/scrapyseleniumtest/scrapyseleniumtest/middlewares.py
+++ ScrapyProject/scrapyseleniumtest/scrapyseleniumtest/middlewares.py
@@ -5,61 +5,41 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-from scrapy import signals
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import excepted_conditions as EC
+from scrapy.http import HtmlResponse
+from logging import getLogger
 
 
-class ScrapyseleniumtestSpiderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Response, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class ScrapyseleniumtestDownloaderMiddleware(object):
+class SeleniumMiddleware():
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
+    def __init__(self,timeout=None, service_args=[]):
+        self.logger = getLogger(__name__)
+        self.timeout = timeout
+        self.browser = webdriver.PhantomJS(service_args=service_args)
+        self.browser.set_window_size(1400,700)
+        self.browser.set_page_load_timeout(self.timeout)
+        self.wait = WebDriverWait(self.browser, self.timeout)
+
+    def __del__(self):
+        self.browser.close()
+
+
+    def process_request(self, request, spider):
+        """
+        用PhantomJS抓取页面
+        :param request: Request对象
+        :param spider: Spider对象
+        :return: HtmlResponse
+        """
+        self.logger.debug('PhantomJS is Starting')
+        page = 
+        return None    
 
     @classmethod
     def from_crawler(cls, crawler):
