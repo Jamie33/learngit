374ab515f46dd32b28a6db56a8674dfee86689d2
Jamie <452070961@qq.com>
2020-02-14 15:11:19 +0800

    pillow usage

---
+++ Topics/err.py
@@ -0,0 +1,10 @@
+#import logging
+#logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.WARNING)
+import pdb
+
+s = '0'
+n = int(s)
+#logging.info('n = %d' % n)
+pdb.set_trace()
+print(10 / n)
