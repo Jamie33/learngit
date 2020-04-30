7191ed610fa1a31c084cbba6e143bb2ca403de6a
Jamie <452070961@qq.com>
2020-04-30 10:29:44 +0800

    translation file

--- Translation/zh-cn/shipping/flat.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$_LANG['flat']          = '市内快递';
-$_LANG['flat_desc']     = '固定运费的配送方式内容';
-$_LANG['base_fee']      = '基本费用：';
--- Translation/zh-cn/shipping/presswork.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$_LANG['presswork']          = '邮政挂号印刷品';
-$_LANG['presswork_desc']     = '邮政挂号印刷品的描述内容。';
